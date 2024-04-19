b99d722b751d696f0d2802cce3774f75c29ce465
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-04-19 11:09:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,13 +10,13 @@ class Config(object):
     SESSION_NAME = os.environ.get("SESSION_NAME", "merge-bot")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
     PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("mongodb+srv://devilking:devilking@cluster0.evvxofd.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_URL = os.environ.get("DATABASE_URL")
     LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = True
     MODES = ["video-video", "video-audio", "video-subtitle","extract-streams"]
-    UPSTREAM_REPO = "https://github.com/Darkksoul/Videomerge"
+    UPSTREAM_REPO = "https://github.com/ZEUSxCODES/Videomerge"
     UPSTREAM_BRANCH = "main"
 
     START_TEXT = """
